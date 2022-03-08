817daf9de53e13274f2273029b26339501b4cfb2
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2022-03-08 03:19:03 +0100

    completed

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyB4k1kadkpHMzm-s7Ewun9vq2nFAYR907c",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "make-code-easy.firebaseapp.com",
   projectId: "make-code-easy",
   storageBucket: "make-code-easy.appspot.com",
   messagingSenderId: "175841198996",
-  appId: "1:175841198996:web:509bb49065d290fb9e8581",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-6LZ5R1FNG1",
 };
 
